9a217709d35666073679cce00667b7aa91b19ae9
zainabahmed <zainabahmedlll19@gmail.com>
2026-06-26 21:23:58 +0300

    set up

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
+const emojis = ['🍉', '🍎', '🍒', '🥝', '🫐', '🥭']
 
+// const player = ''
+const emojisMix = [...emojis, ...emojis]
 /*------------------------ Cached Element References ------------------------*/
-
+const cardBoard = document.querySelector('.cardboard')
+const carD = document.querySelectorAll('.card')
 /*----------------------------- Event Listeners -----------------------------*/
+//mix cards
+emojisMix.sort(() => Math.random() - 0.5);
+//took this online
+
+
+
+cardBoard.addEventListener('click', function(event){
+    const clickedOn = event.target;
+
+    if (clickedOn.classList.contains('card')) {
+        clickedOn.textContent = emojisMix[clickedOn.id];
+    }
+
+})
 
 /*-------------------------------- Functions --------------------------------*/
